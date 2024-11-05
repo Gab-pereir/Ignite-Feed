9c8022eb6e52652322c40ce0ae37ef7b2b333e15
Gabriel Pereira de Almeida <pessoal.gabriel.pereira@gmail.com>
2024-11-05 11:05:17 -0300

    feat: display posts with formatted date and structured content

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,66 @@
-import { useState } from 'react'
+// App.jsx
+import { useState } from 'react';
 
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
-import { Post } from './components/Post'
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
-import styles from './App.module.css'
-import './global.css'
+import styles from './App.module.css';
+import './global.css';
+import Avatar from './components/Avatar';
 
 function App() {
+    const posts = [
+        {
+            id: 1,
+            author: {
+                avatarUrl: 'https://github.com/Gab-pereir.png',
+                name: 'Gabriel Pereira',
+                role: 'Estagiário em desenvolvimento'
+            },
+            content: [
+                { type: 'paragraph', content: 'Fala galeraa 👋' },
+                { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+                { type: 'link', content: 'jane.design/doctorcare' },
+            ],
+            publishedAt: new Date('2024-11-05 20:00:00'),
+        },
+        {
+            id: 2,
+            author: {
+                avatarUrl: 'https://github.com/Gab-pereir.png',
+                name: 'Gabriel Almeida',
+                role: 'Estagiário em Ux Designer'
+            },
+            content: [
+                { type: 'paragraph', content: 'Fala galeraa 👋' },
+                { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+                { type: 'link', content: 'jane.design/doctorcare' },
+            ],
+            publishedAt: new Date('2024-11-03 20:00:00')
+        },
+    ];
 
-  return (
-    <>
-      <Header />
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <Post />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    {posts.map(post => {
+                        return (
+                            <Post
+                                key={post.id}
+                                author={post.author}
+                                content={post.content}
+                                publishedAt={post.publishedAt}
+                            />
+                        );
+                    })}
+                </main>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,50 +1,56 @@
-import Avatar from './Avatar'
-import { Comment } from './Comment'
-import styles from './Post.module.css'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post() {
+import Avatar from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+export function Post(props) {
+    const publishedDateFormatted = format(props.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(props.publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+
     return (
-        <>
-            <article className={styles.post}>
-                
-                <header>
-                    <div className={styles.author}>
-                        <Avatar src="https://github.com/Gab-Pereir.png" alt="" />
-                        <div className={styles.authorinfo}>
-                            <strong>Gabriel Pereira</strong>
-                            <span>Estagiario em desenvolvimento</span>
-                        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={props.author.avatarUrl} alt="" />
+                    <div className={styles.authorinfo}>
+                        <strong>{props.author.name}</strong>
+                        <span>{props.author.role}</span>
                     </div>
-
-                    <time title='28 de outubro de 2024' dateTime='2024-28-10 13:10:00'>Publicado há 1h</time>
-                </header>
-
-                <div className={styles.content}>
-                    <p>Fala galeraa 👋</p>
-                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                    <p><a href="#">👉 jane.design/doctorcare</a></p>
-                    <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
                 </div>
+                <time title={publishedDateFormatted} dateTime={props.publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
+            </header>
 
-                <form className={styles.commentForm}>
-                    <strong>Deixe seu feedback</strong>
+            <div className={styles.content}>
+                {props.content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>;
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="#">{line.content}</a></p>;
+                    }
+                })}
+            </div>
 
-                    <textarea
-                        placeholder='Deixe um comentario'>
-                    </textarea>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder="Deixe um comentario"></textarea>
+                <button type="submit">Comentar</button>
+            </form>
 
-                    <button type='submit'>Comentar</button>
-                </form>
-
-                <div className={styles.commentList}>
-                    <Comment />
-                    <Comment />
-                    <Comment />
-                </div>
-                
-            </article>
-        </>
-
-
-    )
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+    );
 }
